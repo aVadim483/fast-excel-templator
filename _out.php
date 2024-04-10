e375cb008cbf1769a015a24279a3213226d3927b
aVadim <vadim483@gmail.com>
2024-04-11 01:04:54 +0300

    formulas in row templates

--- _out.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo '<head>';
-include __DIR__ . '/_styles.php';
-echo '</head>';
-echo '<body>';
-echo "<table style='width: 100%'><tr><td style='width: 50%;vertical-align: top;'>$tpl<br>$html1</td><td style='width: 50%;vertical-align: top;'>$out<br>$html2</td></tr></table>";
-echo '</body>';
